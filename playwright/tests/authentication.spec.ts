0b51d76cfcb11bd0d6639cf55adaf9c2c832b56e
cornshuckin2 <43525061+cornshuckin2@users.noreply.github.com>
2024-09-29 22:55:28 -0400

    Locked out user + suggestions

--- playwright/tests/authentication.spec.ts
+++ playwright/tests/authentication.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 import { URL } from "../support";
 
 test.describe("Login scenarios", () => {
-  test("can login", async ({ page }) => {
+  test("can login", { tag: "@P1" }, async ({ page }) => {
     await test.step("Navigate to login page", async () => {
       await page.goto(URL);
       await expect(page).toHaveTitle(/Swag Labs/);
@@ -20,6 +20,24 @@ test.describe("Login scenarios", () => {
       await expect(page.getByTestId("primary-header")).toContainText(
         "Swag Labs",
       );
+    });
+  });
+
+  test("cannot login as a locked out user", { tag: "@P1" }, async ({ page }) => {
+    await test.step("Navigate to login page", async () => {
+      await page.goto(URL);
+      await expect(page).toHaveTitle(/Swag Labs/);
+    });
+
+    await test.step("Fill in login form", async () => {
+      await page.getByTestId("username").fill("locked_out_user");
+      await page.getByTestId("password").fill("secret_sauce");
+      await page.getByTestId("login-button").click();
+    });
+
+    await test.step("Verify user is shown an error and remains on the page", async () => {
+      await expect(page.getByTestId("error")).toHaveText("Epic sadface: Sorry, this user has been locked out.")
+      await expect(page).toHaveURL(URL)
     });
   });
 });
