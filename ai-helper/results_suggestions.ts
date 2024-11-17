859e7949fb00da6b5a60cf2e34ac9b84d54238e7
cornshuckin2 <43525061+cornshuckin2@users.noreply.github.com>
2024-11-17 12:23:33 -0700

    Various updates

--- ai-helper/results_suggestions.ts
+++ ai-helper/results_suggestions.ts
@@ -5,7 +5,7 @@ import { getOpenAIResponse } from "../support/openai";
 const OPEN_AI_DESIRED_OUTPUT_FORMAT = [
   "Please format the output into a nicely readable format.",
   "- (table) Suite, Test Case, Priority, Timing, Result",
-  '- (list of code blocks) Give suggestions on missing test cases and their priority for the suites. For missing tests, provide a typescript playwright test block (with priority tag) using psuedo code that can be used to write the test see example: test("cannot login as a locked out user", { tag: "@P1" }, async ({ page }) => { await test.step("Navigate to login page", async () => { await page.goto(URL); await expect(page).toHaveTitle(/Swag Labs/); }); });',
+  '- (list of code blocks) Give suggestions on missing test cases and their priority for the suites. If you believe there are any missing tests, provide a typescript playwright test block (with priority tag) using psuedo code that can be used to write the test see example: test("cannot login as a locked out user", { tag: "@P1" }, async ({ page }) => { await test.step("Navigate to login page", async () => { await page.goto(URL); await expect(page).toHaveTitle(/Swag Labs/); }); });',
   "- (list) Give suggestions on the given results and their priorities (P1 for high priority, P2 for medium priority, P3 for low priority)",
   "- (list) Give suggestions on the given timing of the test cases for the suites",
   "- (summary) Provide a summary of all of the above, including any recommendations.",
