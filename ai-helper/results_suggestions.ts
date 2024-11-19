2cda0cbdd7e17c74ad5a6fb938e7e1446588594c
cornshuckin2 <43525061+cornshuckin2@users.noreply.github.com>
2024-11-19 11:57:05 -0700

    WIP

--- ai-helper/results_suggestions.ts
+++ ai-helper/results_suggestions.ts
@@ -3,7 +3,6 @@ import * as fs from "fs";
 import { getOpenAIResponse } from "../support/openai";
 
 const OPEN_AI_DESIRED_OUTPUT_FORMAT = [
-  "If there are no test results, only return a message of 'Summary unavailable, no test results found'.",
   "Please format the output into a nicely readable format.",
   "- (table) Suite, Test Case, Priority, Timing, Result",
   '- (list of code blocks) Give suggestions on missing test cases and their priority for the suites. If you believe there are any missing tests, provide a typescript playwright test block (with priority tag) using psuedo code that can be used to write the test see example: test("cannot login as a locked out user", { tag: "@P1" }, async ({ page }) => { await test.step("Navigate to login page", async () => { await page.goto(URL); await expect(page).toHaveTitle(/Swag Labs/); }); });',
@@ -16,6 +15,13 @@ main();
 
 async function main() {
   const testResults = await getTestResults();
+  if (!testResults || testResults.length === 0) {
+    fs.writeFileSync(
+      "script_output.txt",
+      "Summary unavailable, no test results found",
+    );
+    return;
+  }
   const suggestions = await getChatGPTSuggestions(
     OPEN_AI_DESIRED_OUTPUT_FORMAT + "\n" + JSON.stringify(testResults),
   );
